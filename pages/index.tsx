8b19a3d11e514adbb7e0aa257b537796b2600a3c
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-30 01:25:11 +0530

    completed layout the project now auth.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "../components/Header"
 
 export default function Home() {
-  return <div className="text-3xl text-sky-500">Hello World!</div>;
+  return(
+    <>
+      <Header label="Home"/>
+    </>
+  ) 
 }

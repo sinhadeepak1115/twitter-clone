90975b962dfa90d2a40ad8f315517f8cfedf1999
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-29 20:31:05 +0530

    making components for the project.

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,8 @@
 import { BsHouseFill, BsBellFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi"
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
   const items = [
@@ -19,7 +22,25 @@ const Sidebar = () => {
       icon: FaUser,
     },
   ];
-  return <div className="col-span-1 h-full pr-4 md:pr-6"></div>;
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout"/>
+          <SidebarTweetButton/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

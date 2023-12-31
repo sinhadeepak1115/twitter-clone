81d1431822cb6a6cbe3fbbc0b3969f367f116e28
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2023-12-31 12:55:30 +0530

    making componets of the file.

---
+++ components/Modal.tsx
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: ()=> void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+}
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled
+
+}) => {
+    const handleClose = useCallback(() =>{
+        if (disabled){
+            return;
+        }
+        
+    },[])
+return (
+    <div></div>
+);
+};
+export default Modal;

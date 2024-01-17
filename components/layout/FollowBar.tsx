250ea35deb476dc872aabbee14f191fc293ea651
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-18 03:55:54 +0530

    added user images.

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -17,6 +17,9 @@ const FollowBar = () => {
               <Avatar userId={user.id} />
               <div className="flex flex-col">
                 <p className="text-white font-semibold text-sm">{user.name}</p>
+                <p className="text-neutral-400 text-sm">
+                  @{user.username}
+                </p>
               </div>
             </div>
           ))}

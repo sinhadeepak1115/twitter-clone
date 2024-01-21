c1382c30c4603013dbc020d3d7ae033d20f20526
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-22 02:42:25 +0530

    made profile photo of the user.

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,10 @@ import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps {
   label: string;
-  shrowBackArrow?: boolean;
+  showBackArrow?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ label, shrowBackArrow }) => {
+const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
   const router = useRouter();
   const handleBack = useCallback(() => {
     router.back();
@@ -17,16 +17,16 @@ const Header: React.FC<HeaderProps> = ({ label, shrowBackArrow }) => {
     <div className="border-b-[1px] border-neutral-800 p-5">
       <div className="flex flex-row items-center gap-2">
         {
-            shrowBackArrow && (
-                <BiArrowBack 
-                onClick={handleBack}
-                color="white"
-                size={20}
-                className="
+          showBackArrow && (
+            <BiArrowBack
+              onClick={handleBack}
+              color="white"
+              size={20}
+              className="
                 cursor-pointer
                 hover:opacity-70
                 transition"/>
-            )
+          )
         }
         <h1 className="text-white text-xl font-semibold">{label}</h1>
       </div>
